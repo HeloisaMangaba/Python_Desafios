63cd19ef133b206e77fb2186e6708fdc338f04f0
HeloisaMangaba <mangabahelo@gmail.com>
2023-02-26 18:09:38 -0300

    tres desafios

--- Desafio01.py
+++ Desafio01.py
@@ -1,5 +1,5 @@
 ## Heloísa M Coletti
-# DESAFIO 01 Leia o nome de uma pessoa e mostre uma mensagem de boas vindas de acordo com o valor digitado.
+# DESAFIO 01 - Leia o nome de uma pessoa e mostre uma mensagem de boas vindas de acordo com o valor digitado.
 
 nome = input('Qual o seu nome? ')
 print('Seja bem vindo(a)', nome, ', é um prazer telo(a) aqui!')
--- Desafio02.py
+++ Desafio02.py
@@ -1,5 +1,5 @@
 ## Heloísa M Coletti
-# DESAFIO 02 Leia o dia, mês e ano de nascimento de uma pessoa e mostre uma mensagem com a data formatada.
+# DESAFIO 02 - Leia o dia, mês e ano de nascimento de uma pessoa e mostre uma mensagem com a data formatada.
 
 dia = input('Digite o dia do seu nascimento: ')
 mes = input('E qual o mês: ')
