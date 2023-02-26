7af7b9855d4f232157c89dab935580ae2182b5d4
HeloisaMangaba <mangabahelo@gmail.com>
2023-02-26 18:00:57 -0300

    3 desafios

---
+++ Desafio02.py
@@ -0,0 +1,8 @@
+## Heloísa M Coletti
+# DESAFIO 02 - Leia o dia, mês e ano de nascimento de uma pessoa e mostre uma mensagem com a data formatada.
+
+dia = input('Digite o dia do seu nascimento: ')
+mes = input('E qual o mês: ')
+ano = input('Agora digite o ano: ')
+
+print('Você nasceu no dia', dia, 'de', mes, 'de', ano,)
---
+++ Desafio03.py
@@ -0,0 +1,8 @@
+## Heloísa M Coletti
+# DESAFIO 03 - Leia dois números e mostre o resultado entre eles.
+
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite um segundo número: '))
+soma = n1+n2
+
+print('A soma ente os números', n1, 'e', n2, 'é:', soma,)
